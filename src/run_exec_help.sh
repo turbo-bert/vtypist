cd5937c37371f8ef9dd90cb5f8f24e4a766c73fd
Robert Degen <robert.degen@mailbox.org>
2024-03-03 13:04:10 +0100

    ...

---
+++ src/run_exec_help.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+clear
+vp -m vtypist "$@" --help
+read
--- src/vtypist/__main__.py
+++ src/vtypist/__main__.py
@@ -1,10 +1,33 @@
+import time
+import sys
 import subprocess
+import argparse
 
-dom = "win2k22"
-srcfile = "keys"
+import rich
+from rich.pretty import pprint as PP
+from rich.console import Console
+from rich.table import Table
+CONSOLE = Console()
+
+
+parser = argparse.ArgumentParser(prog="vtypist", description="Typing via virsh made 'easier'. Implementation is incomplete. Default Keyboard Type is 'de'.", epilog="Useful? Donate: https://www.paypal.com/donate/?hosted_button_id=4EZE2QKKG29JE THANK YOU")
+parser.add_argument("host", metavar="HOST", type=str, help="HOST for virsh (see 'virsh --list')")
+parser.add_argument("filename", metavar="FILENAME", type=str, help="Filename or '-' for STDIN")
+parser.add_argument("--de", action="store_true", help="(default) german layout")
+parser.add_argument("--en", action="store_true", help="english layout")
+parser.add_argument("--do-cad", action="store_true", help="press CTRL-ALT-DELETE (same en/de)")
+parser.add_argument("--do-win-cmd", action="store_true", help="assume windows - start a shell")
+
+
+args = parser.parse_args()
+
+
+dom = args.host
+srcfile = args.filename
 src=None
 
 #KEY_LEFTCTRL KEY_ESC
+#KEY_LEFTCTRL KEY_LEFTALT KEY_DELETE
 
 # translation table
 tt = {}
@@ -39,11 +62,28 @@ for c in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
 tt['y'] = 'KEY_Z'
 tt['Y'] = 'KEY_LEFTSHIFT KEY_Z'
 
-with open(srcfile, 'r') as f:
-    src = f.read().replace("\r", "")
+if srcfile != "-":
+    with open(srcfile, 'r') as f:
+        src = f.read().replace("\r", "")
 
-for c in src:
-    if c in tt:
-        cmd = """virsh send-key %s %s""" % (dom, tt[c])
-        print(cmd)
-        subprocess.call(cmd, shell=True)
+
+def run_v(h, param):
+    cmd = """virsh send-key %s %s""" % (dom, tt[c])
+    print(cmd)
+    subprocess.call(cmd, shell=True)
+
+
+def run_v_tt(h, src):
+    global tt
+    for c in src:
+        if c in tt:
+            run_v(dom, tt[c])
+
+
+if args.do_win_cmd:
+    run_v(args.host, "KEY_LEFTCTRL KEY_ESC")
+    time.sleep(2)
+    run_v_tt(args.host, "cmd.exe\n")
+    sys.exit(0)
+
+run_v_tt(dom, src)
